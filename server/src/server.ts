60617e2c6475bd9af35282f06afd535079653470
Harshhu045 <upadhyayh886@gmail.com>
2025-10-27 23:58:13 +0530

    some changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,8 +33,8 @@ app.use('/api/blog', blogRouter)
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
-    console.log('server is running on port ' + PORT)
-})
+// app.listen(PORT, () => {
+//     console.log('server is running on port ' + PORT)
+// })
 
 export default app;
