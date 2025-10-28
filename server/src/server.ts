1ccf5d26a5eee6ac40eddd5b3afa3e39d694fc43
Harshhu045 <upadhyayh886@gmail.com>
2025-10-28 18:03:25 +0530

    slash remove

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
   origin: ["http://localhost:5173",
-    "https://auto-scribe-ashy.vercel.app/"
+    "https://auto-scribe-ashy.vercel.app"
   ],
   credentials: true
 }));
